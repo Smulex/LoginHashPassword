fd3f5c11d9cabe1bf1feca296403848b5479d8f1
Christian Bøgh <chri45i4@zbc.dk>
2021-01-05 12:20:43 +0100

    Done

---
+++ LoginHashPassword/LoginHashPassword/Hash.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginHashPassword
+{
+    public static class Hash
+    {
+        private const int HasingIterationsCount = 50000;
+        private const int HashByteSize = 24;
+
+        public static byte[] GenerateSalt()
+        {
+            const int saltLength = 32;
+
+            using (RNGCryptoServiceProvider randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[saltLength];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+            //byte[] b = new byte[0];
+            //return b;
+        }
+
+        //private static byte[] Combine(byte[] first, byte[] second)
+        //{
+        //    byte[] ret = new byte[first.Length + second.Length];
+
+        //    Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+        //    Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+
+        //    return ret;
+        //}
+
+        //public static byte[] HashPasswordWithSalt(byte[] toBeHashed, byte[] salt)
+        //{
+        //    using (SHA256 sha256 = SHA256.Create())
+        //    {
+        //        return sha256.ComputeHash(Combine(toBeHashed, salt));
+        //    }
+        //}
+        internal static byte[] HashPasswordWithSalt(string password, byte[] salt, int iterations = HasingIterationsCount, int hashByteSize = HashByteSize)
+        {
+            Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            return pbkdf2.GetBytes(hashByteSize);
+        }
+    }
+}
---
+++ LoginHashPassword/LoginHashPassword/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginHashPassword
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection myConnection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LoginHashPasswordDB;Integrated Security=True");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBTN_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (usernameTXT.Text == string.Empty || passwordTXT.Password == string.Empty)
+                {
+                    MessageBox.Show("Fill in all the fields");
+                }
+                else if (CheckForUser(usernameTXT.Text))
+                {
+                    myConnection.Open();
+
+                    string query = "SELECT password, salt FROM [User] Where username = @username";
+
+                    SqlCommand selectCommand = new SqlCommand(query, myConnection);
+                    selectCommand.Parameters.AddWithValue("@username", usernameTXT.Text);
+                    selectCommand.ExecuteNonQuery();
+
+                    SqlDataReader sReader;
+
+                    sReader = selectCommand.ExecuteReader();
+
+                    while (sReader.Read())
+                    {
+                        if (sReader["password"].ToString() == Convert.ToBase64String(Hash.HashPasswordWithSalt(passwordTXT.Password, Convert.FromBase64String(sReader["salt"].ToString()))))
+                        {
+                            MessageBox.Show("Login successful!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Wrong password");
+                        }
+                    }
+
+                    myConnection.Close();
+                }
+                else
+                {
+                    MessageBox.Show("User doesn't exists!");
+                }
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Login error" + error);
+            }
+        }
+
+        private void RegisterBTN_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (usernameTXT.Text == string.Empty || passwordTXT.Password == string.Empty)
+                {
+                    MessageBox.Show("Fill in all the fields");
+                }
+                else if (!CheckForUser(usernameTXT.Text))
+                {
+                    byte[] salt = Hash.GenerateSalt();
+
+                    byte[] password = Hash.HashPasswordWithSalt(passwordTXT.Password, salt);
+
+                    myConnection.Open();
+
+                    string query = "INSERT INTO [User] VALUES(@username, @password, @salt);";
+
+                    SqlCommand selectCommand = new SqlCommand(query, myConnection);
+                    selectCommand.Parameters.AddWithValue("@username", usernameTXT.Text);
+                    selectCommand.Parameters.AddWithValue("@password", Convert.ToBase64String(password));
+                    selectCommand.Parameters.AddWithValue("@salt", Convert.ToBase64String(salt));
+                    selectCommand.ExecuteNonQuery();
+
+                    myConnection.Close();
+
+                    MessageBox.Show("User is made");
+                }
+                else
+                {
+                    MessageBox.Show("User already exists!");
+                }
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Errors during creation" + error);
+            }
+        }
+
+        private bool CheckForUser(string username)
+        {
+            myConnection.Open();
+
+            string query = "SELECT username FROM [User];";
+
+            SqlCommand selectCommand = new SqlCommand(query, myConnection);
+            selectCommand.ExecuteNonQuery();
+
+            SqlDataReader sReader;
+
+            sReader = selectCommand.ExecuteReader();
+
+            while (sReader.Read())
+            {
+                if (sReader["username"].ToString() == username)
+                {
+                    myConnection.Close();
+                    return true;
+                }
+            }
+
+            myConnection.Close();
+
+            return false;
+        }
+    }
+}
